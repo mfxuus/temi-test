a97b875b6bbd5458e14d3bfb26fbbf3bb94168ae
Michael Xu <michael_fxu@hotmail.com>
2025-02-25 10:08:08 -0600

    revert back to saving images

--- app/src/main/java/com/example/temitest/MainActivity.kt
+++ app/src/main/java/com/example/temitest/MainActivity.kt
@@ -242,69 +242,69 @@ class MainActivity : ComponentActivity(),
         val imageCapture = imageCapture ?: return
 
         // Create time stamped name and MediaStore entry.
-//        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-//            .format(System.currentTimeMillis())
-//        val contentValues = ContentValues().apply {
-//            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-//            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-//            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-//                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-//            }
-//        }
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
 //
 //        // Create output options object which contains file + metadata
-//        val outputOptions = ImageCapture.OutputFileOptions
-//            .Builder(contentResolver,
-//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-//                contentValues)
-//            .build()
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
 
         // Set up image capture listener, which is triggered after photo has
         // been taken
         // ==Not working on Temi==
-        imageCapture.takePicture(
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageCapturedCallback() {
-                private fun ByteBuffer.toByteArray(): ByteArray {
-                    rewind()    // Rewind the buffer to zero
-                    val data = ByteArray(remaining())
-                    get(data)   // Copy the buffer into a byte array
-                    return data // Return the byte array
-                }
-
-                private fun ByteArray.toBase64(): String =
-                    Base64.Default.encode(this)
-
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onCaptureSuccess(image: ImageProxy){
-                    val buffer = image.planes[0].buffer
-                    val data = buffer.toByteArray().toBase64()
-                    image.close()
-                    val msg = "Photo capture succeeded."
-                    Log.d(TAG, msg)
-                    Log.i("BASE64", data);
-                }
-            }
-        )
 //        imageCapture.takePicture(
-//            outputOptions,
 //            ContextCompat.getMainExecutor(this),
-//            object : ImageCapture.OnImageSavedCallback {
+//            object : ImageCapture.OnImageCapturedCallback() {
+//                private fun ByteBuffer.toByteArray(): ByteArray {
+//                    rewind()    // Rewind the buffer to zero
+//                    val data = ByteArray(remaining())
+//                    get(data)   // Copy the buffer into a byte array
+//                    return data // Return the byte array
+//                }
+//
+//                private fun ByteArray.toBase64(): String =
+//                    Base64.Default.encode(this)
+//
 //                override fun onError(exc: ImageCaptureException) {
 //                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
 //                }
 //
-//                override fun
-//                        onImageSaved(output: ImageCapture.OutputFileResults){
-//                    val msg = "Photo capture succeeded: ${output.savedUri}"
-//                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+//                override fun onCaptureSuccess(image: ImageProxy){
+//                    val buffer = image.planes[0].buffer
+//                    val data = buffer.toByteArray().toBase64()
+//                    image.close()
+//                    val msg = "Photo capture succeeded."
 //                    Log.d(TAG, msg)
+//                    Log.i("BASE64", data);
 //                }
 //            }
 //        )
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
     }
 
     private fun captureVideo() {}
@@ -327,7 +327,7 @@ class MainActivity : ComponentActivity(),
             imageCapture = ImageCapture.Builder()
                 .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY) // Fastest capture
 //                .setTargetResolution(Size(1280, 720))
-                .setJpegQuality(65) // Adjust quality to avoid buffer overflows
+                .setJpegQuality(80) // Adjust quality to avoid buffer overflows
                 .build()
 
             // Select back camera as a default
